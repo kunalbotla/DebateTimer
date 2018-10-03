a48ca6b8810e49b021fc1e78b11a5b0a1bbce30b
Kunal Botla <kunalbotla@gmail.com>
2018-10-03 11:37:02 -0400

    New Version (Another Remake)

---
+++ New/Debate Timer/Debate Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Debate Timer
+//
+//  Created by Kunal Botla on 9/26/18.
+//  Copyright © 2018 Kunal Botla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ New/Debate Timer/Debate TimerTests/Debate_TimerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Debate_TimerTests.swift
+//  Debate TimerTests
+//
+//  Created by Kunal Botla on 9/26/18.
+//  Copyright © 2018 Kunal Botla. All rights reserved.
+//
+
+import XCTest
+@testable import Debate_Timer
+
+class Debate_TimerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
