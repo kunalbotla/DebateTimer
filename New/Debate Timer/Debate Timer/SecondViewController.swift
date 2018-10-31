fab182afa48219fb200c70f110c084fcca1d2f1b
Kunal Botla <kunalbotla@gmail.com>
2018-10-31 09:20:42 -0400

    Second View Controller Added/Updates

---
+++ New/Debate Timer/Debate Timer/SecondViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SecondViewController.swift
+//  Debate Timer
+//
+//  Created by Kunal Botla on 10/24/18.
+//  Copyright © 2018 Kunal Botla. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var navbarTimer: UINavigationBar!
+    
+    @IBAction func buttonFullReset(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var labelSpeech: UILabel!
+    
+    @IBOutlet weak var labelTimer: UILabel!
+    
+    @IBAction func buttonPauseResume(_ sender: Any) {
+    }
+    
+    @IBAction func buttonReset(_ sender: Any) {
+    }
+    
+    @IBAction func buttonStart(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- New/Debate Timer/Debate Timer/ViewController.swift
+++ New/Debate Timer/Debate Timer/ViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var navbarHome: UINavigationBar!
+    
+    @IBAction func buttonReset(_ sender: Any) {
+    }
+    
+    @IBAction func buttonCase(_ sender: Any) {
+    }
+    
+    @IBAction func buttonRebttal(_ sender: Any) {
+    }
+    
+    @IBAction func buttonCrossFire(_ sender: Any) {
+    }
+    
+    @IBAction func buttonSummary(_ sender: Any) {
+    }
+    
+    @IBAction func buttonFinalFocus(_ sender: Any) {
+    }
+    
+    @IBAction func buttonProPrep(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var labelProPrep: UILabel!
+    
+    @IBAction func buttonConPrep(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var labelConPrep: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
